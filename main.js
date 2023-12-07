503932dd4317099c5b479d0e99bb508d661f5afe
PlebianJim <108274011+PlebianJim@users.noreply.github.com>
2023-12-07 16:56:34 +0000

    final commit

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
 //Get all elements with the "column" class name
 let elements = document.getElementsByClassName("column");
 
-//Declare a loop variable
-let i = 0;
-
 //Full-width images
-function fullWidth() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "100%";
-  }
+function one() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "100%";
+    }
 }
 
 //Two images side by side
-function twoImagesSideBySide() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "50%";
-  }
+function two() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "50%";
+    }
 }
 
 //Four images side by side
-function fourImagesSideBySide() {
-  for (i = 0; i < elements.length; i++) {
-    elements[i].style.flex = "25%";
-  }
+function four() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "25%";
+    }
+}
+
+
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
 }
